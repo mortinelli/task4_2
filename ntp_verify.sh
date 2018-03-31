d9d47631cb33e00d2c6fd58f22e0193db837cf94
mortinelli <seer_os@mail.ru>
2018-03-31 19:53:34 +0300

    Change verify: copy bak to conf

--- ntp_verify.sh
+++ ntp_verify.sh
@@ -11,7 +11,6 @@ diff -u /etc/ntp.conf.bak /etc/ntp.conf 1>/dev/null
 if [[ $? != "0" ]] ; then
 echo NOTICE: /etc/ntp.conf was changed. Calculated diff:
 diff -u /etc/ntp.conf.bak /etc/ntp.conf > /tmp/diff.out ; cat /tmp/diff.out
-sed -i '/pool*/d' /etc/ntp.conf
-echo pool ua.pool.ntp.org >> /etc/ntp.conf
+cp /etc/ntp.conf.bak /etc/ntp.conf
 service ntp restart
 fi
